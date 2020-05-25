e69a5e66e9998aa45d6761b1b632a4627408f999
mishrapranav <mishrapranav002@gmail.com>
2020-05-25 13:39:04 +0530

    Initial commit inisde the code editor project

---
+++ js/shortcuts.js
@@ -0,0 +1,14 @@
+$(window).bind("keydown", function(event){
+     
+ 
+     if(event.ctrlKey)
+     {
+         switch(String.fromCharCode(event.which).toLowerCase())
+         {
+             case 'n':
+                 event.preventDefault();
+                 createNewFile();
+                 break;
+         }
+     }
+})
